0c950d6144c250a0bb0cb6b10ca39ef0b8984d82
Janardhan Chalamala <164723129+GH2023003716@users.noreply.github.com>
2026-05-15 11:12:21 +0530

    Add files via upload

---
+++ FaceRecognition/app/src/main/java/org/tensorflow/lite/examples/detection/WebServices/ConstantString.java
@@ -0,0 +1,17 @@
+package org.tensorflow.lite.examples.detection.WebServices;
+
+/**
+ * Created by DNS4NIC on 6/13/2018.
+ */
+
+public class ConstantString {
+
+    public final static String URL = "http://10.36.169.110:5000/api";
+
+    public static final String FACEDETECT = "facedetect";
+
+    public final static String GETFACEDETECT_URL = URL + "/" +  FACEDETECT;
+
+}
+
+
